5a7edb002a709ab5b8ab68b3a6a01d5b9b0c903b
Devin Carter <the.devin.carter@outlook.com>
2017-06-10 16:45:24 -0500

    updated course URL

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/golf_guru', function(req, res) {
     res.send({ 'Oak Hollow': 'http://127.0.0.1:3000/golf_guru/courses/oak-hollow' });
 });
 
-app.get('/golf_guru/courses/oak-hollow/info', function(req, res) {
+app.get('/golf_guru/courses/oak-hollow', function(req, res) {
     var options = { root: __dirname }
 
     res.sendFile('oak-hollow.json', options);
