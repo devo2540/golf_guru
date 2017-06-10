59659c7e9048e454b04452d82e3fa113c2706336
Devin Carter <the.devin.carter@outlook.com>
2017-06-10 15:14:41 -0500

    server return app list with links

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World!')
+    res.json({ 'golf_guru': 'http://127.0.0.1:3000/golf_guru' });
 });
+
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
