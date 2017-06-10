b819a4768a072eedacfb5637f3f9c199b21861f1
Devin Carter <the.devin.carter@outlook.com>
2017-06-10 15:24:52 -0500

    add course list

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.get('/', function(req, res) {
     res.json({ 'golf_guru': 'http://127.0.0.1:3000/golf_guru' });
 });
 
+app.get('/golf_guru', function(req, res) {
+    res.send({ 'Oak Hollow': 'http://127.0.0.1:3000/golf_guru/courses/oak-hollow' });
+});
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
